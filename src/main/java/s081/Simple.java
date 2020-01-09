89f2203c274795b9492d17b5737895aaefe38d6d
Laura Marino <lauramarino1993@gmail.com>
2020-01-09 17:29:24 +0100

    9/01

--- src/main/java/s081/Simple.java
+++ src/main/java/s081/Simple.java
@@ -1,42 +1,59 @@
 package s081;
 
 public class Simple {
-    public int negate(int value) {
-        if (value == Integer.MIN_VALUE) {
-            throw new IllegalArgumentException("Can't negate MIN_VALUE");
-        }
-        return -value;
-    }
+	public int negate(int value) {// essendoci un certo spazio di memoria, rendere positivo il min value di un
+									// intero non ci starebbe all'interno della memoria. es. lo short va da -128 a
+									// 127, se io rendo positivo -128 va fuori.
+		// è un unchecked perchè non è specificato il metodo throws
+		if (value == Integer.MIN_VALUE) {
+			throw new IllegalArgumentException("Can't negate MIN_VALUE");
+		}
+		return -value;
+	}
 
-    public int negate2(int value) throws MyNegateException {
-        if (value == Integer.MIN_VALUE) {
-            throw new MyNegateException(Integer.MAX_VALUE);
-        }
-        return -value;
-    }
-    
-    public static void main(String[] args) {
-        Simple simple = new Simple();
-        System.out.println("Negate " + Integer.MIN_VALUE);
-        try {
-            int x = simple.negate2(Integer.MIN_VALUE);
-            System.out.println("My value negated is: " + x);
-        } catch(MyNegateException mne) {
-            System.out.println("My value negated is close to: "
-                    + mne.getAlternativeValue());            
-        }
-    }
+	public int negate2(int value) throws MyNegateException {// è checked perchè enuncia l'eccezione fatta da noi sotto. 
+		if (value == Integer.MIN_VALUE) {
+			throw new MyNegateException(Integer.MAX_VALUE);
+		}
+		return -value;
+	}
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("no value");
+			return;
+		}
+
+		int value= 0;
+		
+		try{
+			value = Integer.parseInt(args[0]);
+		} catch(NumberFormatException nfe) {
+			System.out.println("Pass me an integer");
+			return;
+		}
+		
+		Simple simple = new Simple();
+		System.out.println("Negate " + value);
+
+		try {
+			int x = simple.negate2(value);
+			System.out.println("My value negated is: " + x);
+		} catch (MyNegateException mne) {
+			System.out.println("My value negated is close to: " + mne.getAlternativeValue());
+		}
+	}
 }
 
 class MyNegateException extends Exception {
-    private static final long serialVersionUID = 1L;
-    private int alternativeValue;
-    
-    public MyNegateException(int alternativeValue) {
-        this.alternativeValue = alternativeValue;
-    }
-    
-    public int getAlternativeValue() {
-        return alternativeValue;
-    }
+	private static final long serialVersionUID = 1L;
+	private int alternativeValue;
+
+	public MyNegateException(int alternativeValue) {
+		this.alternativeValue = alternativeValue;
+	}
+
+	public int getAlternativeValue() { //metodo get: ha lo scopo di rendere disponibile una proprietà che è privata. in questo caso è l'unico modo per leggerlo, e nessuno lo modifica più
+		return alternativeValue;
+	}
 }
